0191f568954c36e256e53c57e8dc824861954f56
devatomicfull <devfull.atomic@gmail.com>
2025-10-24 10:03:23 -0300

    feat(activity_animal_info.xml) foi Adicionado o design do layou de tela das informações dos animais!
    - Foi diretamente criado a estrutura da activity para as Informações dos animais porem como informado apenas o design foi implementado

---
+++ bootcamp11_Animals_App/app/src/main/java/github/devatomicfull/bootcamp11_animals_app/ActivityAnimal_Info.java
@@ -0,0 +1,15 @@
+package github.devatomicfull.bootcamp11_animals_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityAnimal_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_info);
+
+    }
+}
