0b9b8246724500a4fad57faf7beb57be9007782c
devatomicfull <devfull.atomic@gmail.com>
2025-10-27 20:51:09 -0300

    feat - Adicionado no adapter as partes para que seja dinamico a tela de tarefas - criado a classe DatabaseHelper onde vamos extender a SQLiteOpenHelper

---
+++ bootcamp12_Lista_De_Tarefas/app/src/main/java/github/devatomicfull/bootcamp12_lista_de_tarefas/DatabaseHelper.java
@@ -0,0 +1,4 @@
+package github.devatomicfull.bootcamp12_lista_de_tarefas;
+
+public class DatabaseHelper {
+}
--- bootcamp12_Lista_De_Tarefas/app/src/main/java/github/devatomicfull/bootcamp12_lista_de_tarefas/NoteAdapter.java
+++ bootcamp12_Lista_De_Tarefas/app/src/main/java/github/devatomicfull/bootcamp12_lista_de_tarefas/NoteAdapter.java
@@ -1,17 +1,61 @@
 package github.devatomicfull.bootcamp12_lista_de_tarefas;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NoteAdapter extends RecyclerView.Adapter {
+import java.util.ArrayList;
 
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private ArrayList<Note> notes;
+    private Context context;
+
+    public NoteAdapter(ArrayList<Note> notes, Context context) {
+        this.notes = notes;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.note_holder, viewGroup, false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder noteHolder, int i) {
+        noteHolder.textViewTitulo.setText(notes.get(i).getTitle());
+        noteHolder.textViewDescricao.setText(notes.get(i).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
 
     class NoteHolder extends RecyclerView.ViewHolder{
+        private TextView textViewTitulo;
+        private TextView textViewDescricao;
+        private ImageButton imgEdit;
 
         public NoteHolder(@NonNull View itemView) {
             super(itemView);
+
+            textViewTitulo = itemView.findViewById(R.id.textView_Titulo);
+            textViewDescricao = itemView.findViewById(R.id.textView_Sub_Texto);
+            imgEdit = itemView.findViewById(R.id.imgEdit);
         }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void atualizar(){
+        notifyDataSetChanged();
     }
 }
